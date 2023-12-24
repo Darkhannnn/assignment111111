70a99ba4f3ae1b4ebfb870b4fe5c3901ebe7ea0f
Darkhannnn <230621@astanait.edu.kz>
2023-12-24 14:33:46 +0600

    added classes point and shape

---
+++ src/models/Shape.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Shape {
+
+    // container of Points (e.g. ArrayList<Point>)
+
+//     public void addPoint(Point x, Point y){
+
+//     }
+//    - adds to the container
+
+    // calculatePerimeter()
+
+    // getAverageSide()
+
+    // getLongestSide()
+}
